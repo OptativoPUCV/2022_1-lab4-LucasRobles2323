7a9fb051f10f69151d8ea16d78f968224fbce69d
lucas.robles <you@example.com>
2022-04-08 22:26:55 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -151,7 +151,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-
+    map->current++;
     while (map->current < map->capacity)
     {
         if( (map->buckets[map->current] != NULL) && (map->buckets[map->current]->key != NULL) ) {
